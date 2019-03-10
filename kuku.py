fb6d5a3e6eb33c64f21e5df61eaab5e5f3a84cb3
Sato <s_ychm@yahoo.co.jp>
2019-03-10 12:43:26 +0900

    kuku

---
+++ kuku.py
@@ -0,0 +1,41 @@
+number = 1
+
+for numbers in range(1, 10):
+    print(number * numbers, end=' ')
+
+# number = 2
+# print()
+# for numbers in range(1, 10):
+#     print(number * numbers, end=' ')
+number += 1
+print()
+for numbers in range(1, 10):
+    print(number * numbers, end=' ')
+number += 1
+print()
+for numbers in range(1, 10):
+    print(number * numbers, end=' ')
+number += 1
+print()
+for numbers in range(1, 10):
+    print(number * numbers, end=' ')
+number += 1
+print()
+for numbers in range(1, 10):
+    print(number * numbers, end=' ')
+number += 1
+print()
+for numbers in range(1, 10):
+    print(number * numbers, end=' ')
+number += 1
+print()
+for numbers in range(1, 10):
+    print(number * numbers, end=' ')
+number += 1
+print()
+for numbers in range(1, 10):
+    print(number * numbers, end=' ')
+number += 1
+print()
+for numbers in range(1, 10):
+    print(number * numbers, end=' ')
